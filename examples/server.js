2e6370ff8d1a1d54bc26eaecf7eac104555aa50e
ijse <i@ijser.cn>
2014-05-05 11:43:33 +0800

    fix server response mime

--- examples/server.js
+++ examples/server.js
@@ -3,24 +3,25 @@ var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
 	if(req.url === '/lib/anthStats.js') {
-		res.setHeader("Content-Type", "application/x-javascript")
+		res.setHeader("Content-Type", "application/x-javascript");
 		fs.createReadStream(__dirname + '/../lib/anthStats.js').pipe(res);
-		return;
-	}
-
-	if(/^\/(index.html)?/.test(req.url)) {
-		res.setHeader("Content-Type", "text/html")
-		fs.createReadStream(__dirname + '/static/index.html').pipe(res);
 		return;
 	}
 
 	if(/^\/stat/.test(req.url)) {
 		console.log("Got stat: ", req.url);
 
-		res.setHeader("Content-Type", "image/jpeg")
+		res.setHeader("Content-Type", "image/jpeg");
 		res.end();
 		return;
 	}
+
+	if(/^\/(index.html)?/.test(req.url)) {
+		res.setHeader("Content-Type", "text/html");
+		fs.createReadStream(__dirname + '/static/index.html').pipe(res);
+		return;
+	}
+
 });
 server.listen(8000);
 
